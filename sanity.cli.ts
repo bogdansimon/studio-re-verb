ff376dd3e589f84dde5de4117076dd4e8b83d582
Bogdan Simon <simonbogdandorin@gmail.com>
2025-07-11 20:33:33 +0200

    add studio host

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,11 +3,13 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'qms6docm',
-    dataset: 'production'
+    dataset: 'production',
   },
+  studioHost: 're-verb',
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
    */
+
   autoUpdates: true,
 })
